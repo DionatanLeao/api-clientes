0550c6260d46485ae435bd333de4eab9f0ed64c2
DionatanLeao <dionatan.ti@outlook.com>
2020-07-20 14:03:30 -0300

    Deletando um cliente

--- src/main/java/com/api/clientes/controller/ClienteController.java
+++ src/main/java/com/api/clientes/controller/ClienteController.java
@@ -1,15 +1,13 @@
 package com.api.clientes.controller;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,12 +29,21 @@ public class ClienteController {
 	public Cliente save(@RequestBody @Valid Cliente cliente) {
 		return repository.save(cliente);
 	}
-	
+
 	@GetMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Cliente findById(@PathVariable Integer id) {
 		return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 	
-		
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		repository.findById(id).map(cliente -> {
+			repository.delete(cliente);
+			return Void.TYPE;
+		})
+		.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
 }
