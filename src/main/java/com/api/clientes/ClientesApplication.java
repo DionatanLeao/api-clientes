063a2ea7e0130bce5390f2b45ab25c31f9a99cdd
DionatanLeao <dionatan.ti@outlook.com>
2020-07-28 15:02:58 -0300

    Concluindo a validação de clientes

--- src/main/java/com/api/clientes/ClientesApplication.java
+++ src/main/java/com/api/clientes/ClientesApplication.java
@@ -1,24 +1,10 @@
 package com.api.clientes;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.api.clientes.entity.Cliente;
-import com.api.clientes.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ClientesApplication {
-
-	@Bean
-	public CommandLineRunner run(@Autowired ClienteRepository repository) {
-		return args -> {
-			Cliente cliente = Cliente.builder().cpf("00000000000").nome("Fulano").build();
-			repository.save(cliente);
-		};
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
